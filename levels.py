2a83363429952347dfd993eceb6792f147912104
TorySnopl <Paramedic064@gmail.com>
2026-01-12 15:33:04 -0800

    created file for cat object, started level 1

---
+++ levels.py
@@ -0,0 +1,10 @@
+from utils import clear, pause, slow_print
+from state import game_state
+
+def level_one(game_state):
+    clear()
+    slow_print("Episode 1: The Anomaly")
+    pause()
+
+    slow_print("You wake up to a quiet morning. The sun is just beginning to rise, casting a warm glow over your surroundings. Everything seem normal, but slightly too still.")
+    
